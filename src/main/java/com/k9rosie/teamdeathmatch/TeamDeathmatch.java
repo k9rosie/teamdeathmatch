078f7cab47e1db1aced3aec45cf9d0d35ed6e9f3
k9rosie <k9rosie@gmail.com>
2016-07-27 15:49:26 -0500

    created gamemode code

---
+++ src/main/java/com/k9rosie/teamdeathmatch/TeamDeathmatch.java
@@ -0,0 +1,51 @@
+package com.k9rosie.teamdeathmatch;
+
+import com.k9rosie.novswar.NovsWar;
+import com.k9rosie.novswar.gamemode.Gamemode;
+
+public class TeamDeathmatch implements Gamemode {
+    private TeamDeathmatchPlugin plugin;
+    private NovsWar novswar;
+
+    private int gameTime;
+    private int deathTime;
+    private int maxScore;
+    private String gamemodeName;
+
+    public TeamDeathmatch(TeamDeathmatchPlugin plugin) {
+        this.plugin = plugin;
+        gameTime = 600;
+        deathTime = 5;
+        maxScore = 120;
+        gamemodeName = "tdm";
+    }
+
+    public void initialize() {
+
+    }
+
+    public int getGameTime() {
+        return gameTime;
+    }
+
+    public int getDeathTime() {
+        return deathTime;
+    }
+
+    public int getMaxScore() {
+        return maxScore;
+    }
+
+    public String getGamemodeName() {
+        return gamemodeName;
+    }
+
+    public void setNovswar(NovsWar novswar) {
+        this.novswar = novswar;
+    }
+
+    public NovsWar getNovsWar() {
+        return novswar;
+    }
+
+}
---
+++ src/main/java/com/k9rosie/teamdeathmatch/listener/ServerListener.java
@@ -0,0 +1,32 @@
+package com.k9rosie.teamdeathmatch.listener;
+
+import com.k9rosie.novswar.NovsWar;
+import com.k9rosie.novswar.event.NovsWarInitializationEvent;
+import com.k9rosie.novswar.event.NovsWarPlayerKillEvent;
+import com.k9rosie.teamdeathmatch.TeamDeathmatch;
+import com.k9rosie.teamdeathmatch.TeamDeathmatchPlugin;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+
+public class ServerListener implements Listener {
+    private TeamDeathmatchPlugin plugin;
+    private TeamDeathmatch tdm;
+
+    public ServerListener(TeamDeathmatchPlugin plugin) {
+        this.plugin = plugin;
+        tdm = plugin.getGamemode();
+    }
+
+    @EventHandler(priority = EventPriority.HIGHEST)
+    public void onNovsWarInitialize(NovsWarInitializationEvent event) {
+        plugin.getLogger().info("hooking gamemode into novswar");
+        tdm.setNovswar(event.getNovsWar());
+        event.getNovsWar().getGamemodes().put(tdm.getGamemodeName(), tdm);
+    }
+
+    @EventHandler(priority = EventPriority.NORMAL)
+    public void onNovsWarPlayerKill(NovsWarPlayerKillEvent event) {
+        event.getKillerTeam().incrementScore();
+    }
+}
